53be704a3daf1ed97d1ec8813c4052e99f34868f
Daniel <Daniel@RAMPAGE>
2017-11-05 12:04:58 -0300

    1- Más datos en juego de pruebas 2- Error en getClave, devulve la misma clave para dos coord. diferentes    ej: -34.290906, -56.394002 ; -34.065572, -56314966

--- ObligatorioA2/src/PaqueteSistema/Testing.java
+++ ObligatorioA2/src/PaqueteSistema/Testing.java
@@ -24,22 +24,36 @@ public class Testing {
 		assertEquals(Retorno.Resultado.ERROR_4, s.registrarProductor("4.798.128-7", "Daniel", "Copacabana M31 S12", "daniel.r.23@gmail.com", "098369129").resultado);		
 		
 		//Registro 1 ciudad
-		assertEquals(Retorno.Resultado.OK, s.registrarCiudad("Mendoza Chico",-34.2417138,-56.2194872).resultado);
-		assertEquals(Retorno.Resultado.ERROR_2, s.registrarCiudad("Mendoza Chico",-34.2417138,-56.2194872).resultado);
+		assertEquals(Retorno.Resultado.OK, s.registrarCiudad("Veinticinco de Mayo",-34.192082,-56.339053).resultado);
+		assertEquals(Retorno.Resultado.ERROR_2, s.registrarCiudad("Veinticinco de Mayo",-34.192082,-56.339053).resultado);
+		assertEquals(Retorno.Resultado.OK, s.registrarCiudad("Berrondo",-34.145863,-56.277579).resultado);
+		assertEquals(Retorno.Resultado.OK, s.registrarCiudad("Florida",-34.094821,-56.220934).resultado);
+		assertEquals(Retorno.Resultado.OK, s.registrarCiudad("Mendoza",-34.281350,-56.213361).resultado);
+		assertEquals(Retorno.Resultado.OK, s.registrarCiudad("Mendoza Chico",-34.237845,-56.218210).resultado);
+		assertEquals(Retorno.Resultado.OK, s.registrarCiudad("Cardal",-34.290906,-56.394002).resultado);
 		
 		//Registro 1 plantacion
-		assertEquals(Retorno.Resultado.OK, s.registrarPlantacion("Plantacion_1",-34.211054,-56.2192116,"4.798.128-7",3000).resultado);
-		assertEquals(Retorno.Resultado.ERROR_2, s.registrarPlantacion("Plantacion_1",-34.211054,-56.2192116,"4.798.128-7",0).resultado);
-		assertEquals(Retorno.Resultado.ERROR_3, s.registrarPlantacion("Plantacion_2",-34.211054,-56.2192116,"4.317.139-7",3000).resultado);
+		assertEquals(Retorno.Resultado.OK, s.registrarPlantacion("Plantacion_1",-34.176316,-56.175714,"4.798.128-7",3000).resultado);
+		assertEquals(Retorno.Resultado.ERROR_2, s.registrarPlantacion("Plantacion_1",-34.176316,-56.175714,"4.798.128-7",0).resultado);
+		assertEquals(Retorno.Resultado.ERROR_3, s.registrarPlantacion("Plantacion_2",-34.176316,-56.175714,"4.317.139-7",3000).resultado);
+		assertEquals(Retorno.Resultado.OK, s.registrarPlantacion("Plantacion_2",-34.065572,-56.314966,"4.868.799-1",3000).resultado);
 		
 		//Registro 1 silo
-		assertEquals(Retorno.Resultado.OK, s.registrarSilo("Silo_1",-34.235199,-56.2185438,300000).resultado);
-		assertEquals(Retorno.Resultado.ERROR_2, s.registrarSilo("Silo_1",-34.235199,-56.2185438,0).resultado);
-		assertEquals(Retorno.Resultado.ERROR_3, s.registrarSilo("Silo_2",-34.235199,-56.2185438,300000).resultado);
+		assertEquals(Retorno.Resultado.OK, s.registrarSilo("Silo_1",-34.102376,-56.122201,300000).resultado);
+		assertEquals(Retorno.Resultado.ERROR_2, s.registrarSilo("Silo_1",-34.102376,-56.122201,0).resultado);
+		assertEquals(Retorno.Resultado.ERROR_3, s.registrarSilo("Silo_2",-34.102376, -56.122201,300000).resultado);
+		
 		
 		//Registro 2 tramo
-		assertEquals(Retorno.Resultado.OK, s.registrarTramo(-34.211054,-56.2192116,-34.235199,-56.2185438,100).resultado); //plantacion - silo
-		assertEquals(Retorno.Resultado.OK, s.registrarTramo(-34.211054,-56.2192116,-34.2417138,-56.2194872,100).resultado); //plantacion - ciudad 
+		assertEquals(Retorno.Resultado.OK, s.registrarTramo(-34.094821,-56.220934,-34.102376,-56.122201,11).resultado); //ciudad florida - silo
+		assertEquals(Retorno.Resultado.OK, s.registrarTramo(-34.176316,-56.175714,-34.094821,-56.220934,14).resultado); //plantacion - ciudad florida
+		assertEquals(Retorno.Resultado.OK, s.registrarTramo(-34.145863,-56.277579,-34.094821,-56.220934,8).resultado); //ciudad berrondo - ciudad florida
+		assertEquals(Retorno.Resultado.OK, s.registrarTramo(-34.192082,-56.339053,-34.145863,-56.277579,13).resultado); //ciudad veinticinco de mayo - ciudad berrondo
+		assertEquals(Retorno.Resultado.OK, s.registrarTramo(-34.290906,-56.394002,-34.192082,-56.339053,16).resultado); //ciudad cardal - ciudad veinticinco de mayo
+		assertEquals(Retorno.Resultado.OK, s.registrarTramo(-34.192082,-56.339053,-34.281350,-56.213361,20).resultado); //ciudad veinticinco de mayo - ciudad mendoza
+		assertEquals(Retorno.Resultado.OK, s.registrarTramo(-34.281350,-56.213361,-34.237845,-56.218210,5).resultado); //ciudad mendoza - ciudad mendoza chico
+		assertEquals(Retorno.Resultado.OK, s.registrarTramo(-34.237845,-56.218210,-34.176316,-56.175714,10).resultado); //ciudad mendoza chico - plantacion 1
+		assertEquals(Retorno.Resultado.OK, s.registrarTramo(-34.094821,-56.220934,-34.065572,-56.314966,16).resultado); //ciudad florida - plantacion 2
 		
 		//assertEquals(Retorno.Resultado.OK, s.eliminarPunto(-34.211054,-56.2192116).resultado);
 		
